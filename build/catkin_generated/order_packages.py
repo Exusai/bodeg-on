b77fae2007cb4b09a3d8e775166fe2c69b23118a
Faber <a.a.r.samuel99@gmail.com>
2022-02-14 21:14:27 -0600

    repo init

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/faber/bodeg-on/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/faber/Unity-Robotics-Hub/tutorials/pick_and_place/ROS/devel;/home/faber/bodeg-on/devel;/home/faber/EK_AutoNOMOS_Sim/devel;/opt/ros/melodic'.split(';') if '/home/faber/Unity-Robotics-Hub/tutorials/pick_and_place/ROS/devel;/home/faber/bodeg-on/devel;/home/faber/EK_AutoNOMOS_Sim/devel;/opt/ros/melodic' != '' else []
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export LD_LIBRARY_PATH='/home/faber/Unity-Robotics-Hub/tutorials/pick_and_place/ROS/devel/lib:/home/faber/EK_AutoNOMOS_Sim/devel/lib:/opt/ros/melodic/lib'
+export PKG_CONFIG_PATH='/home/faber/Unity-Robotics-Hub/tutorials/pick_and_place/ROS/devel/lib/pkgconfig:/home/faber/EK_AutoNOMOS_Sim/devel/lib/pkgconfig:/opt/ros/melodic/lib/pkgconfig'
+export PWD='/home/faber/bodeg-on/build'
